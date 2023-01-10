bd6a538f77176ae0689e8711cf569c441eba4f09
Turgay Levent <turgayleventt@gmail.com>
2023-01-11 00:01:41 +0300

    student_list

--- student_api/urls.py
+++ student_api/urls.py
@@ -1,8 +1,12 @@
 
 from django.urls import path
-from .views import home
+from .views import (
+    home,
+    students_list,
+)
 
 urlpatterns = [
     path("", home),
+    path("student-list/", students_list, name='list'),
 
 ]
